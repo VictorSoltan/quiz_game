74e06add75ff524226a0b5997aa7bda4769cc0d2
VictorSoltan <victor.soltan.evg@gmail.com>
2022-11-01 18:12:11 +0200

    done

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}> 
+    <Router> 
   		<App />
     </Router>
   </React.StrictMode>
